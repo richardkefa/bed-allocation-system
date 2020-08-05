5ca5ffe7acb9048bfe02fa5fe4c3e4da28bba23b
George Wayne Mwangi <waynegeorge193.com>
2020-08-05 18:07:50 +0300

    append booking details to checkout section.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ var bedPrice=parseInt(getBedPrice.options[getBedPrice.selectedIndex].value);
 var getWardPrice=document.getElementById("ward");
 var wardPrice=parseInt(getWardPrice.options[getWardPrice.selectedIndex].value);
 var totalPrice= bedPrice + wardPrice;
-alert(totalPrice)
+alert(totalPrice);
+return totalPrice=bedPrice + wardPrice;
 }
 function Patient (name,number,details) {
   this.patientName=name;
@@ -31,11 +32,19 @@ $(document).ready(function() {
     var inputtedHospital = $(this).find("select#hospital").val();
     var inputtedBedType = $(this).find("select#bed").val();
     var inputtedWard = $(this).find("select#ward").val();
-    var newBooking = new Hospital  (name,bed,ward);
+    var newBooking = new Hospital  (inputtedHospital,inputtedBedType,inputtedWard);
     newPatient.bookingDetails.push(newBooking)
     console.log(newPatient.patientName);
 
 
-})
-})
+    $(".patient-name").append(newPatient.patientName);
+    $(".hospital-name").append(newBooking.hospitalName);
+    $(".bed-type").append(newBooking.bedType);
+    $(".total-price").append(getValue());
+
+
+});
+});
+
+
 })
